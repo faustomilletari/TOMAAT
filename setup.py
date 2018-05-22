cd77ff799079fc202fafcc4856ddb0eb766b51f5
Fausto Milletari <fmilletari@nvidia.com>
2018-05-22 14:46:09 -0700

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='tomaat',
-    version="0.0.1",
+    version="0.1.0",
     description='TOMAAT SERVER SIDE',
+    url='https://github.com/fmilletari/tomaat',
+    author='Fausto Milletari',
+    author_email='tomaat.segmentation@gmail.com',
     packages=find_packages(),
 )
