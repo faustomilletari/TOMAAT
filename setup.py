f222e94fdc48918141d38e526f813a2cafb1cafb
Fausto Milletari <fmilletari@nvidia.com>
2018-05-25 16:20:16 -0700

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tomaat',
-    version="0.1.1",
+    version="0.1.2",
     description='TOMAAT SERVER SIDE',
     url='https://github.com/fmilletari/tomaat',
     author='Fausto Milletari',
