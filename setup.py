243b13fccdc8e4de66fc5e712250dad6867ce837
Fausto Milletari <fmilletari@nvidia.com>
2018-07-31 17:26:34 -0700

    advanced version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tomaat',
-    version="0.1.4",
+    version="0.1.5",
     description='TOMAAT SERVER SIDE',
     url='https://github.com/fmilletari/tomaat',
     author='Fausto Milletari',
